69363505ef1137d789ec90364aac02eaa1942cad
Princeton J. Rose <princetonjrose@gmail.com>
2023-05-02 15:06:12 -0400

    finished update to work with class instance instead

--- lib/song.py
+++ lib/song.py
@@ -1,10 +1,11 @@
+from typing import Any
 from . import CONN, CURSOR
 import ipdb
 
 class Song:
 
-    def __init__ ( self, name, album ) :
-        self.id = None
+    def __init__ ( self, name, album, id=None ) :
+        self.id = id
         self.name = name
         self.album = album
     
@@ -32,7 +33,8 @@ class Song:
     def find( self, id ) :
         songs = CURSOR.execute( f'select * from songs where id = {id} ' )
         song = [ song_into_dict( song ) for song in songs ]
-        return song[0] if song else 'Could not find that record.'
+        song_instance = Song( name = song[0]['name'], album = song[0]['album'], id = song[0]['id'] )
+        return song_instance if song else 'Could not find that record.'
     
     def save ( self ) :
         # sql = """
@@ -42,7 +44,7 @@ class Song:
         # CURSOR.execute( sql, ( self.name, self.album ) )
         CURSOR.execute( f"insert into songs ( name, album ) values ( '{ self.name }', '{ self.album }' )" )
         self.id = CURSOR.execute( 'select last_insert_rowid() from songs' ).fetchone()[0]
-        return Song.find( self.id )
+        return self
 
     @classmethod
     def create ( cls, name, album ) :
@@ -61,17 +63,13 @@ class Song:
     # update doesn't work right now for some reason
     def update ( self, **attributes ) :
         song = Song.find( self.id )
-        
-        if type( song ) == dict :
+        if song and type( song ) == Song :
             for key in attributes:
-                if song[key] and key is not 'id' :
-                    self[key] = attributes[key]
+                if song.__getattribute__( key ) and key is not 'id' :
+                    setattr( self, key, attributes[key] )
             CURSOR.execute( f"update songs set name = '{ self.name }', album = '{ self.album }' where id = { self.id }; " )
             updated_song = Song.find( self.id )
             return updated_song
         else :
             return song
-
-
-
 
